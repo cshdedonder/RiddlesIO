ae5e341a210825fb1c54b91cbf4cdcbb36ffc6f5
cdedonder <cedric.dedonder@gmail.com>
2019-02-28 20:42:26 +0100

    started TicTacToe

---
+++ src/main/scala/cshdedonder/riddlesio/tictactoe/RandomTicTacToeBot.scala
@@ -0,0 +1,8 @@
+package cshdedonder.riddlesio.tictactoe
+
+import cshdedonder.riddlesio.Bot
+
+object RandomTicTacToeBot extends Bot {
+
+  override def handleString(string: String): Option[String] = ???
+}
