599b67dac6444ecfe6fa23792dbd3b366e78a6de
cdedonder <cedric.dedonder@gmail.com>
2019-03-03 17:32:07 +0100

    minor update

--- src/main/scala/cshdedonder/riddlesio/tictactoe/RandomTicTacToeBot.scala
+++ src/main/scala/cshdedonder/riddlesio/tictactoe/RandomTicTacToeBot.scala
@@ -8,14 +8,16 @@ object RandomTicTacToeBot extends Bot {
 
   override def handle(string: String): (Bot, Option[String]) = string match {
     case s if s.startsWith("settings") => (this, None)
-    case s if s.startsWith("update game field") => (new Bot {
+    case s if s.startsWith("update game field") => (getSubBot(s), None)
+  }
 
-      val validPlaces: Array[(Int, Int)] = ???
+  private def getSubBot(s: String): Bot = new Bot {
 
-      override def handle(string: String): (Bot, Option[String]) = string match {
-        case t if t.startsWith("action move") => (RandomTicTacToeBot, Some("%1$d %2$d" format validPlaces(Random.nextInt(validPlaces.length))))
-        case t => RandomTicTacToeBot.handle(t)
-      }
-    }, None)
+    val validPlaces: Array[(Int, Int)] = ???
+
+    override def handle(string: String): (Bot, Option[String]) = string match {
+      case t if t.startsWith("action move") => (RandomTicTacToeBot, Some("%1$d %2$d" format validPlaces(Random.nextInt(validPlaces.length))))
+      case t => RandomTicTacToeBot.handle(t)
+    }
   }
 }
